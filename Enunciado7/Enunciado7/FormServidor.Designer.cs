0646579f628569cdef0cd351fa289d8ae8814e42
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2020-02-16 23:19:14 +0100

    Add files via upload

---
+++ Enunciado7/Enunciado7/FormServidor.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Enunciado7
+{
+    partial class FormServidor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtSocket = new System.Windows.Forms.TextBox();
+            this.lbConexiones = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(12, 122);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HOST";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SOCKET";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(12, 31);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 20);
+            this.txtHost.TabIndex = 3;
+            // 
+            // txtSocket
+            // 
+            this.txtSocket.Location = new System.Drawing.Point(12, 96);
+            this.txtSocket.Name = "txtSocket";
+            this.txtSocket.Size = new System.Drawing.Size(100, 20);
+            this.txtSocket.TabIndex = 4;
+            // 
+            // lbConexiones
+            // 
+            this.lbConexiones.FormattingEnabled = true;
+            this.lbConexiones.Location = new System.Drawing.Point(130, 21);
+            this.lbConexiones.Name = "lbConexiones";
+            this.lbConexiones.Size = new System.Drawing.Size(658, 251);
+            this.lbConexiones.TabIndex = 5;
+            // 
+            // FormServidor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 283);
+            this.Controls.Add(this.lbConexiones);
+            this.Controls.Add(this.txtSocket);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "FormServidor";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtSocket;
+        private System.Windows.Forms.ListBox lbConexiones;
+    }
+}
+
---
+++ Enunciado7/Enunciado7/FormServidor.cs
@@ -0,0 +1,66 @@
+﻿using ClasesSocket;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enunciado7
+{
+    public delegate void EnviarConcatenacion(ListBox lb);
+    public delegate void NuevaLinea(ListBox lb, int pos);
+    public partial class FormServidor : Form
+    {
+        Servidor srv;
+        public FormServidor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            srv = new Servidor(txtHost.Text, int.Parse(txtSocket.Text));
+            srv.Start();
+            srv.OnNuevaConexion += Srv_OnNuevaConexion;
+            srv.OnDatosRecibidos += Srv_OnDatosRecibidos;
+        }
+
+        private void Srv_OnDatosRecibidos(string datos, int pos, DateTime fechaDatoRecibido)
+        {
+            lbConexiones.Items[pos] = String.Format("[{0}] en ({1},{2})", srv.GetClientIP(pos), datos, pos);
+            if (srv.Procesos[pos].fechaReciboPrimerDato == null)
+            {
+                srv.Procesos[pos].fechaReciboPrimerDato = DateTime.Now;
+            }
+            else
+            {
+                srv.Procesos[pos].fechaReciboUltimoDato = DateTime.Now;
+            }
+            
+        }
+
+        private void Srv_OnNuevaConexion(string idTerminal, int pos)
+        {
+            NuevaLinea nl = nuevaLinea;
+            lbConexiones.BeginInvoke(nl, new object[] { lbConexiones, pos });
+            srv.Procesos[pos].fechaConexion = DateTime.Now;
+        }
+        public void nuevaLinea(ListBox lb, int pos)
+        {
+            lbConexiones.Items.Add(String.Format("[{0}] en (0,0)", srv.GetClientIP(pos)));
+        }
+        public void enviarConcatenacion(ListBox lb)
+        {
+            String cadena = "";
+            for(int i = 0; i < lb.Items.Count; i++)
+            {
+                String.Concat(cadena, lb.Items[i].ToString() + ";");
+            }
+            srv.EnviarDatos(cadena);
+        }
+    }
+}
